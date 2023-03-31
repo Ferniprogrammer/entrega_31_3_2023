5fd4af5887434221246f41f9d38ac10a3440df28
ferniprogrammer <fsanchor@myuax.com>
2023-03-31 04:07:07 +0200

    crear la clase

--- ej1.py
+++ ej1.py
@@ -1,24 +1,22 @@
 import unidecode
-
-def capicua_o_no (word):
-    word = word.lower()
-    word = word.replace(" ", "")
-    word = unidecode.unidecode(word)
-    word = ''.join(filter(str.isalnum, word))
-    right = len(word) -1
-    left = 0
-    if len(word) % 2 == 0:
-        repeticiones = len(word)//2
-    else:
-        repeticiones = (len(word) -1)//2
-    for _ in range(repeticiones):
-        if word[left] == word[right]:
-            left += 1
-            right -= 1
+class palindrome:
+    def __init__(self, word):
+        self.word = word
+    def is_palindrome(self):
+        word = self.word.lower()
+        word = word.replace(" ", "")
+        word = unidecode.unidecode(word)
+        word = ''.join(filter(str.isalnum, word))
+        right = len(word) -1
+        left = 0
+        if len(word) % 2 == 0:
+            repeticiones = len(word)//2
         else:
-            return False
-    return True
-
-word = str(input("¿ Qué quieres ver si es capicúa o no? "))
-
-print(capicua_o_no(word))
+            repeticiones = (len(word) -1)//2
+        for _ in range(repeticiones):
+            if word[left] == word[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
